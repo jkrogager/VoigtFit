4ad1a31193988ce7ca53f92d06cd10608bb390c9
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2019-06-07 09:36:44 +0200

    exclude VFIT3 in package

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['test_data', 'docs', 'scripts', 'doc']),
+    packages=find_packages(exclude=['test_data', 'docs', 'scripts', 'doc', 'VoigtFit3']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
