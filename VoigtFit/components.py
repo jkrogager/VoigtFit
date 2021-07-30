2af962bbef3668f8f6a537a6fb241a41650fc53c
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2021-07-30 11:42:55 +0200

    Convenience functions for components
    Stand-alone function to generate parameters from best-fit file and from parameter arrays.

--- VoigtFit/components.py
+++ VoigtFit/components.py
@@ -5,6 +5,7 @@ for the overall line profile for the ions.
 """
 
 from numpy import abs
+from lmfit import Parameters
 
 
 class Component(object):
@@ -117,3 +118,76 @@ class Component(object):
     def get_pars(self):
         """Unpack the physical parameters [z, b, logN]"""
         return [self.z, self.b, self.logN]
+
+
+
+def load_components_from_file(fname):
+    """
+    Load best-fit parameters from an output file, ex: 'dataset.fit'
+
+    Parameters
+    ----------
+    fname : str
+        The filename of the VoigtFit output file.
+
+    Returns
+    -------
+    pars : lmfit.Parameters
+        Parameter dictionary of `lmfit.Parameter` instances.
+    """
+    components_to_add = list()
+    with open(fname) as parameters:
+        for line in parameters.readlines():
+            line = line.strip()
+            pars = line.split()
+            if len(line) == 0:
+                pass
+            elif line[0] == '#':
+                pass
+            elif len(pars) == 8:
+                num = int(pars[0])
+                ion = pars[1]
+                z = float(pars[2])
+                z_err = float(pars[3])
+                b = float(pars[4])
+                b_err = float(pars[5])
+                logN = float(pars[6])
+                logN_err = float(pars[7])
+                components_to_add.append([num, ion, z, b, logN,
+                                          z_err, b_err, logN_err])
+
+    pars = Parameters()
+    for comp_pars in components_to_add:
+        (num, ion, z, b, logN, z_err, b_err, logN_err) = comp_pars
+        ion = ion.replace('*', 'x')
+        z_name = 'z%i_%s' % (num, ion)
+        b_name = 'b%i_%s' % (num, ion)
+        N_name = 'logN%i_%s' % (num, ion)
+
+        pars.add(z_name, value=z)
+        pars.add(b_name, value=b)
+        pars.add(N_name, value=logN)
+
+        pars[z_name].stderr = z_err
+        pars[b_name].stderr = b_err
+        pars[N_name].stderr = logN_err
+
+    return pars
+
+
+def components_from_array(ion, *, z, b, logN):
+    """
+    Create a `lmfit.Parameters` dictionary for a given `ion`
+    and arrays/lists of redshift (z), broadening parameter (b) and column density (logN).
+    A component will be generated for each element in the z, b, logN arrays.
+    """
+    pars = Parameters()
+    for num, vals in enumerate(zip(z, b, logN)):
+        z_name = 'z%i_%s' % (num, ion)
+        b_name = 'b%i_%s' % (num, ion)
+        N_name = 'logN%i_%s' % (num, ion)
+
+        pars.add(z_name, value=vals[0])
+        pars.add(b_name, value=vals[1])
+        pars.add(N_name, value=vals[2])
+    return pars
