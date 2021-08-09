adfc3a185abdbff1809d3281879deb46977f836a
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2021-08-09 17:06:07 +0200

    Update __init__.py
    remove forced backend

--- VoigtFit/__init__.py
+++ VoigtFit/__init__.py
@@ -12,12 +12,12 @@ __author__ = 'Jens-Kristian Krogager'
 from os import path
 from sys import version_info
 
-import warnings
-import matplotlib
-# The native MacOSX backend doesn't work for all:
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    matplotlib.use('TkAgg')
+# import warnings
+# import matplotlib
+# # The native MacOSX backend doesn't work for all:
+# with warnings.catch_warnings():
+#     warnings.simplefilter("ignore")
+#     matplotlib.use('TkAgg')
 
 from VoigtFit.container.components import Component
 from VoigtFit.container.dataset import DataSet
