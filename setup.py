b627b779de09a58aa955e25026bdab69df977f16
Jens-Kristian Krogager <jens-kristian@krogager.dk>
2022-05-03 13:54:21 +0200

    No more python2 support

--- setup.py
+++ setup.py
@@ -22,6 +22,9 @@ mypackage_root_dir = path.dirname(path.abspath(__file__))
 with open(path.join(mypackage_root_dir, 'VoigtFit', 'VERSION')) as v_file:
     version = v_file.read().strip()
 
+# Load Requirements:
+requirements = np.loadtxt('/Users/krogager/coding/VoigtFit/requirements.txt', dtype=str)
+
 if sys.version_info[0] < 3:
     python_version_requirement = '==2.7.*'
     programming_language = 'Programming Language :: Python :: 2.7'
@@ -38,6 +41,8 @@ def _post_install():
     print("\n Please let me know that you're using VoigtFit by filling in the following short form:")
     print(" https://forms.gle/exPEsrPoyfB4Us7w9")
     print(" Thanks for installing VoigtFit!")
+
+
 
 setup(
     name='VoigtFit',
@@ -99,8 +104,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['matplotlib', 'numpy<1.21', 'scipy', 'future',
-                      'lmfit', 'h5py>=2.1', 'astropy', 'numba>=0.51.2'],
+    install_requires=requirements.tolist(),
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
