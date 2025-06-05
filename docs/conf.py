b563f29bc4dbab4058a3b5ea444ac21db1df1cbd
Jens-Kristian Krogager <jens-kristian.krogager@univ-lyon1.fr>
2025-06-05 16:31:41 -0400

    Updated docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -37,11 +37,11 @@ autoclass_content = 'both'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'numpydoc',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
+              'sphinx.ext.coverage',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.autosummary',
+              'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
